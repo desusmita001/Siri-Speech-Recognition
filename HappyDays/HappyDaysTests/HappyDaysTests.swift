65437c7e89e2ca5ab8c4798a9c87e5fcfa264a08
Susmita De <desusmita001@gmail.com>
2025-10-09 21:05:41 -0500

    Initial Commit

---
+++ HappyDays/HappyDaysTests/HappyDaysTests.swift
@@ -0,0 +1,17 @@
+//
+//  HappyDaysTests.swift
+//  HappyDaysTests
+//
+//  Created by Susmita De on 10/9/25.
+//
+
+import Testing
+@testable import HappyDays
+
+struct HappyDaysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
